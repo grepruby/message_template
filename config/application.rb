99ac5d1f66e95599e6631715f78c8d3dae2a6efa
M <mpatel.ror@gmail.com>
2012-11-21 19:49:25 +0530

    PG ERROR

--- config/application.rb
+++ config/application.rb
@@ -60,5 +60,7 @@ module MessageManagement
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    config.middleware.delete ActiveRecord::QueryCache
   end
 end
